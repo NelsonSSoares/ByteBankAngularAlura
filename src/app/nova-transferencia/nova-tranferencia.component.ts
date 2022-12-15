026fd554c4620cb8a055ecd1361b38f7774e5a97
Nelson <punkpunkada@gmail.com>
2022-12-15 15:15:36 -0300

    criando evento do form

--- src/app/nova-transferencia/nova-tranferencia.component.ts
+++ src/app/nova-transferencia/nova-tranferencia.component.ts
@@ -3,9 +3,11 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-nova-transferencia',
     templateUrl: './nova-transferencia.component.html',
-    styleUrls:['./nova-transferencia.component.scss']
+    styleUrls: ['./nova-transferencia.component.scss']
 })
 
-export class NovaTranferenciaComponent{
-
+export class NovaTranferenciaComponent {
+    public transferir() {
+        console.log(`solicitada nova tranferencia`);
+    }
 }
